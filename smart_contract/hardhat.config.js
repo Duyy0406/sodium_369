b42aa2902d75d9fc9e8dec0e342786fdd28ec2ec
Duyy0406 <baoduytran01042006@gmail.com>
2025-08-23 01:43:02 +0800

    update oracle_service

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,13 +1,14 @@
-// hardhat.config.js
-require("@nomicfoundation/hardhat-toolbox");
-require("dotenv").config();
+import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const KURTOSIS_RPC_URL = process.env.KURTOSIS_RPC_URL || "http://127.0.0.1:50740";
 const KURTOSIS_PRIVATE_KEY = process.env.KURTOSIS_PRIVATE_KEY;
 const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
+const config = {
   solidity: "0.8.28",
   networks: {
     sepolia: {
